a0c7a0d68664d35fd3e34ac18e921ceec6231d78
amagolden <amagoldenstein@gmail.com>
2024-10-25 15:19:12 -0500

    adding msal auth

--- src/dashboards/SampleDashboard.jsx
+++ src/dashboards/SampleDashboard.jsx
@@ -5,15 +5,66 @@ import ListWidget from "../widgets/ListWidget";
 import SampleWidget from "../widgets/SampleWidget";
 import SampleAuthWidget from "../widgets/SampleAuthWidget";
 
-export default class SampleDashboard extends BaseDashboard {
-  layout() {
+import React, { useState } from 'react';
+
+import { PageLayout } from '../PageLayout';
+import { loginRequest } from '../internal/authConfig';
+import { callMsGraph } from '../services/graph';
+import { ProfileData } from '../ProfileData';
+
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
+
+import '../App.css';
+
+import Button from 'react-bootstrap/Button';
+
+  /**
+* Renders information about the signed-in user or a button to retrieve data about the user
+*/
+  function ProfileContent() {
+    const { instance, accounts } = useMsal();
+    const [graphData, setGraphData] = useState(null);
+
+    function RequestProfileData() {
+        // Silently acquires an access token which is then attached to a request for MS Graph data
+        if (accounts[0]) {  // Check if accounts[0] exists
+          instance
+              .acquireTokenSilent({
+                  ...loginRequest,
+                  account: accounts[0],
+              })
+              .then((response) => {
+                  callMsGraph(response.accessToken).then((response) => setGraphData(response));
+              });
+      }
+  };
+
     return (
       <>
-        <ListWidget />
-        <ChartWidget />
-        <SampleWidget />
-        <SampleAuthWidget />
+          <h5 className="card-title">Welcome {accounts[0]?.name || "User"}</h5> {/* Fallback to "User" if name is undefined */}
+          <br/>
+          {graphData ? (
+              <ProfileData graphData={graphData} />
+          ) : (
+              <Button variant="secondary" onClick={RequestProfileData}>
+                  Request Profile Information
+              </Button>
+          )}
       </>
-    );
+    )
   }
-}
+
+  export default class SampleDashboard extends BaseDashboard {
+    layout() {
+      return (
+        <>
+          <PageLayout />
+          <ListWidget />
+          <ChartWidget />
+          <SampleWidget />
+          <SampleAuthWidget />
+          <ProfileContent />
+        </>
+      );
+    }
+  }
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,23 @@
 import "./index.css";
-
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './internal/authConfig';
+
+const msalInstance = new PublicClientApplication(msalConfig);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+
+/**
+ * We recommend wrapping most or all of your components in the MsalProvider component. It's best to render the MsalProvider as close to the root as possible.
+ */
+ root.render(
+  <React.StrictMode>
+      <MsalProvider instance={msalInstance}>
+          <App />
+      </MsalProvider>
+  </React.StrictMode>
+);
